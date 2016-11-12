923e28bd58ef6aa00d0cd14d3f3c01e1bd941dd7
otarim <otarim.com@gmail.com>
2016-11-12 21:58:45 +0800

    增加离线微信rss，增加图片离线

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ var cheerio = require('cheerio'),
     nodemailer = require('nodemailer'),
     smtpTransport = require('nodemailer-smtp-transport'),
     archiver = require('archiver'),
-    config = require('./config')
+    config = require('./config'),
+    crypto = require('crypto'),
+    http = require('http'),
+    spawn = require('child_process').spawn
 
 const DIST_PATH = path.resolve(__dirname, './out')
+const IMG_PATH = path.resolve(__dirname, './out/imgs')
+
+const STORE = require(path.resolve(__dirname, './store.json'))
 
 var queue = function(q, opt) {
     opt = opt || {}
@@ -70,6 +76,22 @@ var fetch = function(opt) {
     })
 }
 
+var filterFetched = function(items, {key, cate}) {
+    cate = md5(cate)
+    STORE[cate] = STORE[cate] || []
+    items = items.filter(function(item) {
+        return STORE[cate].indexOf(item[key]) === -1
+    })
+    if (items && items.length) {
+        STORE[cate].push(...items.map(function(item) {
+            return item[key]
+        }))
+        console.log(`更新了${items.length}条记录`)
+        fs.writeFileSync('./store.json', JSON.stringify(STORE))
+    }
+    return items
+}
+
 var parseRss = function(api, formatter) {
     formatter = Object.assign({
         item: 'rss.channel.item',
@@ -88,6 +110,12 @@ var parseRss = function(api, formatter) {
                     body = JSON.parse(parser.toJson(body))
                     body = getRssItem(body, formatter.item)
                     if (body) {
+                        if (formatter.key) {
+                            body = filterFetched(body, {
+                                key: formatter.key,
+                                cate: api
+                            })
+                        }
                         resolve(body.map(function(item) {
                             return {
                                 title: item[formatter.title],
@@ -138,9 +166,70 @@ var genContent = function(re) {
     return buff.join('')
 }
 
+var md5 = function(str) {
+    var md5 = crypto.createHash('md5')
+    md5.update(str)
+    return md5.digest('hex')
+}
+
+// 获取文件名
+var getExt = function(type) {
+    return '.' + type.split('/')[1]
+}
+
+// 获取图片
+var getImg = (function() {
+    try {
+        fs.accessSync(IMG_PATH)
+    } catch (e) {
+        fs.mkdirSync(IMG_PATH)
+    }
+    return function(url) {
+        return new Promise(function(resolve, reject) {
+            http.get(url, function(res) {
+                var filename = md5(url) + getExt(res.headers['content-type']),
+                    filePath = path.resolve(IMG_PATH, filename),
+                    stream = fs.createWriteStream(filePath)
+                stream.on('finish', function() {
+                    resolve({
+                        src: url,
+                        dist: path.join('imgs/', filename)
+                    })
+                })
+                stream.on('error', function(error) {
+                    reject(error)
+                })
+                res.pipe(stream)
+            })
+        })
+    }
+})()
+
+// 下载文章
+var download = function({title, content}) {
+    var $ = cheerio.load(content)
+    var urls = $('img').map(function() {
+        return $(this).attr('src')
+    }).get()
+    return Promise.all(urls.map(getImg))
+}
+
 // kindle推送对于html要求非常严格
-var genFile = function(content) {
-    return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" lang="zh" xml:lang="zh"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><title>Document</title><style>h2{margin-bottom: 2em;}article{margin-bottom: 2em;}img {max-width: 100%;}</style></head><body>${content}</body</body></html>`
+var genFile = function(content, title) {
+    return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" lang="zh" xml:lang="zh"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/><title>${title}</title><style>h2{margin-bottom: 2em;}article{margin-bottom: 2em;}img {max-width: 100%;}</style></head><body>${content}</body</body></html>`
+}
+
+// 调用kindlegen生成mobi文件
+var toMobi = function() {
+    return new Promise(function(resolve, reject) {
+        var task = spawn('sh', ['task.sh'])
+        task.on('close', function() {
+            resolve()
+        })
+        task.stderr.on('data', function(error) {
+            reject(error)
+        })
+    })
 }
 
 // 抓取西贝rss
@@ -154,11 +243,11 @@ var doCnbeta = function() {
         }), {
             limit: 10
         }).then(function(re) {
+            console.log('cnbeta:done')
             return re.ret
         })
     })
 }
-
 
 // 抓取知乎
 var doZhihu = function() {
@@ -166,6 +255,7 @@ var doZhihu = function() {
         item: 'rss.channel.item',
         link: 'description'
     }).then(function(re) {
+        console.log('zhihu:done')
         return re.map(function(item) {
             return {
                 title: item.title,
@@ -175,7 +265,6 @@ var doZhihu = function() {
     })
 }
 
-
 // 抓取v2ex
 var doV2ex = function() {
     return fetch({
@@ -183,6 +272,7 @@ var doV2ex = function() {
     }).then(function(re) {
         if (re) {
             re = JSON.parse(re)
+            console.log('v2ex:done')
             return re.map(function(item) {
                 return {
                     title: item.title,
@@ -204,6 +294,7 @@ var doJiandan = function() {
         }), {
             limit: 10
         }).then(function(re) {
+            console.log('jiandan:done')
             return re.ret
         })
     })
@@ -229,6 +320,39 @@ var doJianshu = function() {
             }))
         }
     }).then(function(re) {
+        console.log('jianshu:done')
+        return re.ret
+    })
+}
+
+// 抓取微信公众号
+var doWeixin = function() {
+    return parseRss('http://rss.iwgc.cn/rss/4868-f9b45aeb355c31816f4210d1142277ad7118',{
+        item: 'rss.channel.item',
+        link: 'description',
+        key: 'pubDate'
+    }).then(function(re) {
+        return queue(re.map(function(item) {
+            // 抓取图片，保存图片
+            return download({
+                title: item.title,
+                content: item.link
+            }).then(function(urls) {
+                var content = item.link
+                urls.forEach(function(url) {
+                    content = content.replace(url.src, url.dist)
+                })
+                return {
+                    title: item.title,
+                    content: content
+                }
+            })
+        }), {
+            limit: 5
+        })
+    })
+    .then(function(re) {
+        console.log('wexin:done')
         return re.ret
     })
 }
@@ -293,7 +417,10 @@ var send = function(attachments) {
 
 // 清理目录
 var clean = function() {
-    walk(DIST_PATH, ['.html','.zip']).forEach(function(file) {
+    walk(DIST_PATH, ['.html','.zip','.mobi']).forEach(function(file) {
+        fs.unlinkSync(file)
+    })
+    walk(IMG_PATH).forEach(function(file) {
         fs.unlinkSync(file)
     })
 }
@@ -307,24 +434,46 @@ process.on('uncaughtException', function(err) {
 
 
 // 入口
-Promise.all([doCnbeta(),doZhihu(),doV2ex(),doJiandan(),doJianshu()]).then(function(res) {
-    var filename = ['cnbeta','zhihu','v2ex','jiandan','jianshu']
-    if (res) {
-        if (!fs.existsSync(DIST_PATH)) {
-            fs.mkdirSync(DIST_PATH)
-        }
-        res.forEach(function(re,index) {
-            fs.writeFileSync(`./out/${filename[index]}.html`, genFile(genContent(re)))
+// Promise.all([doCnbeta(),doZhihu(),doV2ex(),doJiandan(),doJianshu()]).then(function(res) {
+//     var filename = ['cnbeta','zhihu','v2ex','jiandan','jianshu']
+//     console.log('抓取成功')
+//     if (res) {
+//         if (!fs.existsSync(DIST_PATH)) {
+//             fs.mkdirSync(DIST_PATH)
+//         }
+//         res.forEach(function(re,index) {
+//             fs.writeFileSync(`./out/${filename[index]}.html`, genFile(genContent(re)))
+//         })
+//         return Promise.resolve()
+//     }
+// }).then(function() {
+//     archive(walk(DIST_PATH, '.html')).then(function(dist) {
+//         send(dist).then(function(re) {
+//             console.log('发送成功')
+//             clean()
+//         },function(err) {
+//             console.log('发送失败: ' + err)
+//         })
+//     })
+// })
+
+doWeixin().then(function(re) {
+    if (re && re.length) {
+        re.forEach(function(item) {
+            fs.writeFileSync(`./out/${item.title}.html`, genFile(genContent([item]), item.title))
         })
-        return Promise.resolve()
+        return toMobi()
     }
-}).then(function() {
-    archive(walk(DIST_PATH, '.html')).then(function(dist) {
-        send(dist).then(function(re) {
-            console.log('发送成功')
-            clean()
-        },function(err) {
-            console.log('发送失败: ' + err)
+    return Promise.resolve(false)
+}).then(function(signal) {
+    if (signal !== false) {
+        archive(walk(DIST_PATH, '.mobi')).then(function(dist) {
+            send(dist).then(function(re) {
+                console.log('发送成功')
+                clean()
+            },function(err) {
+                console.log('发送失败: ' + err)
+            })
         })
-    })
+    }
 })
